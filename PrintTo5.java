ec3a68c52468361531493f113f8be7a51835ba44
shaked cohen <shakedcohen@shakeds-MacBook-Air.local>
2025-10-30 09:21:14 +0200

    completed part 1 inside demo0 file

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,8 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        int j=0;
+        System.out.println(i);
         // Complete the program's code here:
     }
  }
